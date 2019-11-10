057e1ed36a3abea2a84973fe37f5869d523cef64
Lachlan Marnoch <lachlan.marnoch@students.mq.edu.au>
2019-11-10 18:12:07 +1100

    Added great circle angular distance function.

--- maps/maps.py
+++ maps/maps.py
@@ -3,6 +3,7 @@ from mpl_toolkits.basemap import Basemap
 import numpy as np
 import astropy.table as tbl
 from typing import Union
+from math import *
 
 
 def plot_globe(file, centre_lat=0, centre_lon=0, show=True):
@@ -30,6 +31,32 @@ def plot_map(file, centre_lat=0, centre_lon=0, show=True, projection='cyl'):
         plt.show()
 
     return bmap
+
+
+def great_circle_ang_dist(lon1: float, lat1: float, lon2: float, lat2: float, deg: bool = True):
+    """
+    Calculates the great circle angular distance between two points on the map. All units are interpreted as degrees
+    unless deg is given as False, in which case all units are intepreted as radians.
+    Constructed using https://en.wikipedia.org/wiki/Great-circle_distance
+    :param lon1: Longitude of first point.
+    :param lat1: Latitude of first point.
+    :param lon2: Longitude of second point.
+    :param lat2: Latitude of second points.
+    :param deg:
+    :return:
+    """
+
+    # Convert to radians.
+    if deg:
+        lon1 = radians(lon1)
+        lon2 = radians(lon2)
+        lat1 = radians(lat1)
+        lat2 = radians(lat2)
+
+    delta_lon = lon2 - lon1
+    delta_lat = lat2 - lat1
+
+    return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(delta_lon))
 
 
 location_types = ['city', 'natural']
