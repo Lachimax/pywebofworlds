f6b01cb903661e159fcb275f0c0c5f59042076fe
Lachlan Marnoch <lachlan.marnoch@students.mq.edu.au>
2019-11-24 18:57:36 +1100

    Improvements to read/write

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ from typing import List, Union
 
 
 def sanitise_file_ext(path: str, ext: str = '.csv'):
+    """
+    If the path does not end in the desired extension, appends it.
+    :param path: Path to check.
+    :param ext: Extension to check for and append if not found.
+    :return: Appened path.
+    """
     if ext[0] != '.':
         ext = '.' + ext
     if path[-len(ext):] != ext:
@@ -10,6 +16,14 @@ def sanitise_file_ext(path: str, ext: str = '.csv'):
 
 
 def write_wow_csv(path: str, header: str, names: list, rows: List[str]):
+    """
+    Writes a .csv file with a header specifying the appropriate data type and other information.
+    :param path:
+    :param header:
+    :param names:
+    :param rows:
+    :return:
+    """
     path = sanitise_file_ext(path=path, ext='.csv')
     name_string = ''
     for name in names:
