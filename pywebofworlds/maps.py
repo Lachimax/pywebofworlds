fe6ceff27f86b0bcb7ad8bf9c3542cf908fba776
Lachlan <lachlan.marnoch@students.mq.edu.au>
2023-01-27 11:28:53 +1100

    Additions to travel time

--- pywebofworlds/maps.py
+++ pywebofworlds/maps.py
@@ -11,15 +11,18 @@ except ImportError:
     bmap_available = False
 import numpy as np
 from typing import Union, List
-from math import *
+import numpy as np
 import imageio
 
-from astropy import units as un
+import astropy.units as units
+import astropy.coordinates as coordinates
 import astropy.table as tbl
-
 
 # TODO: Interact directly with SVG?
 # TODO: Stop using basemap and move onto cartopy.
+
+kph = units.km / units.hour
+
 
 def check_basemap():
     if not bmap_available:
@@ -27,17 +30,22 @@ def check_basemap():
     return bmap_available
 
 
-def lon_lat_from_x_y(x: float, y: float, scale=100):
+def lon_lat_from_x_y(
+        x: float,
+        y: float,
+        scale: float = 100
+):
     """
     Take map coordinates and convert them to latitude and longitude. Assumes a cylindrical projection with lat=0,
     long=0 at the centre of the map.
     :param x: x-coordinate on map
     :param y: y-coordinate on map
-    :param scale: The conversion factor between the coordinates on the map and longitude and latitude.
+    :param scale: The conversion factor between the coordinates on the map and longitude and latitude. x and y will be
+    divided by this number.
     :return:
     """
-    lat = y / scale - 90
-    lon = x / scale - 180
+    lon = coordinates.Longitude((x / scale - 180) * units.deg)
+    lat = coordinates.Latitude((y / scale - 90) * units.deg)
 
     return lon, lat
 
@@ -51,8 +59,8 @@ def x_y_from_lon_lat(lat, lon, scale=100):
     :param scale: The conversion factor between the coordinates on the map and longitude and latitude.
     :return:
     """
-    x = (lon + 180) * scale
-    y = (lat + 90) * scale
+    x = (lon.to(units.deg).value + 180) * scale
+    y = (lat.to(units.deg).value + 90) * scale
 
     return x, y
 
@@ -119,7 +127,12 @@ def plot_map(file: str, centre_lat: float = 0, centre_lon: float = 0, show: floa
     return bmap
 
 
-def great_circle_ang_dist(lon1: float, lat1: float, lon2: float, lat2: float, deg: bool = True):
+def great_circle_ang_dist(
+        lon1: coordinates.Longitude,
+        lat1: coordinates.Latitude,
+        lon2: coordinates.Longitude,
+        lat2: coordinates.Latitude,
+):
     """
     Calculates the great circle angular distance between two points on the map, in degrees. All units are interpreted as
     degrees unless deg is given as False, in which case all units are interpreted as radians.
@@ -128,23 +141,20 @@ def great_circle_ang_dist(lon1: float, lat1: float, lon2: float, lat2: float, de
     :param lat1: Latitude of first point.
     :param lon2: Longitude of second point.
     :param lat2: Latitude of second points.
-    :param deg: Interpret units as degrees? If False, interprets as radians.
     :return: float, great circle angular distance, in radians.
     """
-
-    # Convert to radians.
-    if deg:
-        lon1 = radians(lon1)
-        lon2 = radians(lon2)
-        lat1 = radians(lat1)
-        lat2 = radians(lat2)
-
     delta_lon = lon2 - lon1
     # Perform calculation.
-    return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(delta_lon))
+    return np.arccos(np.sin(lat1) * np.sin(lat2) + np.cos(lat1) * np.cos(lat2) * np.cos(delta_lon))
 
 
-def great_circle_distance(lon1: float, lat1: float, lon2: float, lat2: float, radius: float = 6371e3, deg: bool = True):
+def great_circle_distance(
+        lon1: coordinates.Longitude,
+        lat1: coordinates.Latitude,
+        lon2: coordinates.Longitude,
+        lat2: coordinates.Latitude,
+        radius: units.Quantity = 6371e3 * units.m,
+):
     """
     Calculates the great circle distance between two points on the map, in metres. All units are interpreted as degrees
     unless deg is given as False, in which case all units are interpreted as radians.
@@ -153,16 +163,15 @@ def great_circle_distance(lon1: float, lat1: float, lon2: float, lat2: float, ra
     :param lat1: Latitude of first point.
     :param lon2: Longitude of second point.
     :param lat2: Latitude of second points.
-    :param deg: Interpret units as degrees? If False, interprets as radians.
     :param radius: Radius of the planet, in metres.
     :return: float, great circle distance, in metres.
     """
-    if type(radius) is not un.Quantity:
-        radius *= un.m
+    if type(radius) is not units.Quantity:
+        radius *= units.m
     # Calculate great circle angular distance.
-    ang_dist = great_circle_ang_dist(lon1=lon1, lat1=lat1, lon2=lon2, lat2=lat2, deg=deg)
+    ang_dist = great_circle_ang_dist(lon1=lon1, lat1=lat1, lon2=lon2, lat2=lat2)
     # Multiply by radius of planet.
-    return radius * ang_dist
+    return radius * ang_dist.to("rad").value
 
 
 def travel_to(lon: float, lat: float, direction: float, radius: float = 6371e3):
@@ -180,22 +189,52 @@ location_types = ['city', 'natural']
 
 
 class Location:
-    def __init__(self, name: str = None, lon: float = None, lat: float = None, typ: str = None, this_map: "Map" = None,
-                 nxt: "Location" = None, previous: "Location" = None):
+    def __init__(
+            self,
+            name: str,
+            lon: coordinates.Longitude = None,
+            lat: coordinates.Longitude = None,
+            location_type: str = None,
+            this_map: "Map" = None,
+            nxt: "Location" = None,
+            previous: "Location" = None,
+            **kwargs
+    ):
         """
 
         :param name: Name of location.
         :param lon: Longitude of location.
         :param lat: Latitude of location.
-        :param typ: Type of location.
+        :param location_type: Type of location.
         :param this_map: The Map object on which this location belongs.
         :param nxt: The next location. Used when Location is part of a Journey, to link one Location to the next.
         """
         self.name = name
+        self.map = this_map
+        self.x = None
+        self.y = None
+        if lat is None or lon is None:
+            if "x" in kwargs and isinstance(kwargs["x"], float) and "y" in kwargs and isinstance(kwargs["y"], float):
+                x = kwargs["x"]
+                y = kwargs["y"]
+                if self.map is None and "scale" in kwargs:
+                    scale = kwargs["scale"]
+                elif self.map is not None:
+                    scale = self.map.scale
+                else:
+                    scale = 100.
+                lon, lat = lon_lat_from_x_y(x=x, y=y, scale=scale)
+                self.x = x
+                self.y = y
+
+        if lon is not None and not isinstance(lon, coordinates.Longitude):
+            lon = coordinates.Longitude(lon * units.deg)
+        if lat is not None and not isinstance(lat, coordinates.Latitude):
+            lat = coordinates.Latitude(lat * units.deg)
+
         self.lon = lon
         self.lat = lat
-        self.type = typ
-        self.map = this_map
+        self.type = location_type
         if self.map is not None:
             self.map.add_location(self)
 
@@ -212,48 +251,65 @@ class Location:
         :return: float, great circle distance, in metres.
         """
         if self.map is not None:
-            distance = great_circle_distance(lon1=self.lon, lat1=self.lat, lon2=other.lon, lat2=other.lat,
-                                             radius=self.map.planet_radius)
+            distance = great_circle_distance(
+                lon1=self.lon,
+                lat1=self.lat,
+                lon2=other.lon,
+                lat2=other.lat,
+                radius=self.map.planet_radius
+            )
         else:
-            distance = great_circle_distance(lon1=self.lon, lat1=self.lat, lon2=other.lon, lat2=other.lat)
+            distance = great_circle_distance(
+                lon1=self.lon,
+                lat1=self.lat,
+                lon2=other.lon,
+                lat2=other.lat
+            )
+
         return distance
 
-    def travel_time(self, other, speed: float = 4., units: str = "m/s"):
+    def travel_time(self, other, speed: float = 4., unit: str = "m/s"):
         """
 
         :param other:
         :param speed:
-        :param units:
+        :param unit:
         :return:
         """
-        if speed is not un.Quantity:
-            speed = un.Quantity(speed, units)
+        if speed is not units.Quantity:
+            speed = units.Quantity(speed, unit)
         distance = self.distance_to(other)
         time = distance / speed
         return time
 
-    def travel_days(self, other, speed: float = 4., units: str = "km/h", time_per_day: float = 7.5):
+    def travel_days(
+            self,
+            other: 'Location',
+            speed: units.Quantity = 4. * kph,
+            unit: str = "km/h",
+            time_per_day: units.Quantity = 7.5
+    ):
         """
 
         :param other:
         :param speed:
-        :param units:
-        :param time_per_day: In hours.
+        :param unit:
+        :param time_per_day: In hours. Amount of time per day that you will spend travelling.
         :return:
         """
-        if type(speed) is not un.Quantity:
-            speed = speed * un.km / un.hour
+        if type(speed) is not units.Quantity:
+            speed = speed * units.km / units.hour
         else:
-            speed = speed.to(un.km / un.hour)
-        time = self.travel_time(other=other, speed=speed, units=units)
-        if type(time_per_day) is not un.Quantity:
-            time_per_day = time_per_day * un.hour / un.day
+            speed = speed.to(units.km / units.hour)
+        time = self.travel_time(other=other, speed=speed, unit=unit)
+        if type(time_per_day) is not units.Quantity:
+            time_per_day = time_per_day * units.hour / units.day
         else:
-            time_per_day = time_per_day.to(un.hour / un.day)
+            time_per_day = time_per_day.to(units.hour / units.day)
         days = time / time_per_day
-        return days.to(un.day)
+        return days.to(units.day)
 
-    def travel_days_dpd(self, other: 'Location', distance_per_day: un.Quantity = 30 * un.km):
+    def travel_days_dpd(self, other: 'Location', distance_per_day: units.Quantity = 30 * units.km):
         """
         An alternate method for calculating the number of days to travel to another location, by specifying the amount
         of distance covered in a single day.
@@ -262,7 +318,7 @@ class Location:
         :return:
         """
         distance = self.distance_to(other)
-        distance_per_day = distance_per_day.to(un.km)
+        distance_per_day = distance_per_day.to(units.km)
         days = distance / distance_per_day
         return days
 
@@ -271,7 +327,13 @@ marker_colours = ['r', 'g', 'b']
 
 
 class Map:
-    def __init__(self, image: str, locations: str = None, planet_radius: float = 6371e3):
+    def __init__(
+            self,
+            image: str,
+            locations: str = None,
+            planet_radius: units.Quantity = 6371e3 * units.m,
+            scale: float = 100.
+    ):
         """
 
         :param image: Path to image file of map.
@@ -281,14 +343,23 @@ class Map:
         self.image = image
         self.locations_path = locations
         self.planet_radius = planet_radius
+        self.scale = scale
         if locations is not None:
             self.locations_tbl = tbl.Table.read(locations, format='ascii.csv')
         else:
             self.locations_tbl = tbl.Table()
+        self.location_lists = {}
         self.locations = {}
         self.create_locations()
 
-    def add_location(self, location: Location):
+    def global_from_x_y(self, x: float, y: float):
+        return lon_lat_from_x_y(x=x, y=y, scale=self.scale)
+
+    def add_location(
+            self,
+            location: Location,
+            force: bool = True
+    ):
         """
         Add a location to this map's internal list of Locations.
         :param location: Location object.
@@ -296,9 +367,14 @@ class Map:
         """
         # Set location's map pointer to this map.
         location.map = self
-        #
-        self.create_location_type_list(location.type)
-        self.locations[location.type].append(location)
+        if location.name not in self.locations or force:
+            self.locations[location.name] = location
+            self.create_location_type_list(location.type)
+            self.location_lists[location.type].append(location)
+        else:
+            print(f"Location {location.name} already exists in this map.")
+            location = self.locations[location.name]
+        return location
 
     def create_location_type_list(self, typ):
         """
@@ -306,8 +382,8 @@ class Map:
         :param typ: Name of location type
         :return:
         """
-        if typ not in self.locations:
-            self.locations[typ] = []
+        if typ not in self.location_lists:
+            self.location_lists[typ] = []
 
     def create_locations(self):
         """
@@ -316,7 +392,7 @@ class Map:
         """
         for loc in self.locations_tbl:
             print(loc)
-            location = Location(name=loc['name'], lon=loc['longitude'], lat=loc['latitude'], typ=loc['type'])
+            location = Location(name=loc['name'], lon=loc['longitude'], lat=loc['latitude'], location_type=loc['type'])
             self.add_location(location)
 
     def plot_map(self, centre_lat: float = 0, centre_lon: float = 0, projection: str = 'ortho', output: str = None,
@@ -380,12 +456,12 @@ class Map:
         if type(types) is str:
             types = [types]
         elif types is None:
-            types = self.locations.keys()
+            types = self.location_lists.keys()
 
         bmap = self.plot_map(centre_lat=centre_lat, centre_lon=centre_lon, projection=projection, output=None)
 
         for i, typ in enumerate(types):
-            for loc in self.locations[typ]:
+            for loc in self.location_lists[typ]:
                 bmap.plot(loc.lon, loc.lat, f'{marker_colours[i]}o', latlon=True)
                 plt.text(loc.lon, loc.lat, loc.name, c=marker_colours[i], fontsize=fontsize)
         if output is not None:
